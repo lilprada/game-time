33cba62d3e66a7deccc0b5daa788fdbef1b16495
caitlin farrell <farrellcaite@gmail.com>
2020-05-08 13:28:39 -0400

    i still messed it up oop

--- project-1-assessment/js/main.js
+++ project-1-assessment/js/main.js
@@ -1,15 +1,15 @@
-let result = 0;
+let result;
 
 $('<div>').text(`${result}`).appendTo('.header');
 
 
-// $('#addButton').click(function() {
-//     const inputValue = parseInt($('#myInput').val());
-        //add number from total and set it in dom
-//     $("#result").text(result + inputValue);
+$('#addButton').click(function() {
+    const inputValue = parseInt($('#myInput').val());
+        // add number from total and set it in dom
+    $("#result").text(result += inputValue);
 
-//     $('#myInput').val("");
-// });
+    $('#myInput').val("");
+});
 
 //cant figure out how to make this add?
 
@@ -17,15 +17,15 @@ $('<div>').text(`${result}`).appendTo('.header');
 $('#subtractButton').click(function() {
     const inputValue = parseInt($('#myInput').val());
     //subtract number from total and set it in dom
-    $('#result').text(result - inputValue);
+    $('#result').text(result -= inputValue);
 
     $('#myInput').val("");
 });
 
 
-$('#addButton').on('click', (event) => {
-    const inputValue = parseInt($('#myInput').val());
-    $('#result').text(result + inputValue);
+// $('#addButton').on('click', (event) => {
+//     const inputValue = parseInt($('#myInput').val());
+//     $('#result').text(result + inputValue);
 
-    $('#myInput').val("");
-})
+//     $('#myInput').val("");
+// })
